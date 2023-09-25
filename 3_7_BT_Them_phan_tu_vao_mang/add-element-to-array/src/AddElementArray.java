c3e11cd7e8df3f6b84d5414eb8ff2449bba12c9f
huynguyenngocdang <danghuy9229@gmail.com>
2023-09-25 13:47:55 +0700

    commit 3_7

---
+++ 3_7_BT_Them_phan_tu_vao_mang/add-element-to-array/src/AddElementArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AddElementArray {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] numbers = {10, 4, 6, 7, 8};
+
+        String displayOrigin = displayArray(numbers);
+        System.out.println("Origin: " + displayOrigin);
+        System.out.printf("Add at position(0 - %d): ", numbers.length);
+        int indexAdd = scanner.nextInt();
+        if (indexAdd <= numbers.length && indexAdd >= 0) {
+            System.out.println("Add with: ");
+            int valueAdd = scanner.nextInt();
+            int[] newNumbers = arrayReplace(numbers, indexAdd, valueAdd);
+
+            String displayNew = displayArray(newNumbers);
+            System.out.println("New array: " + displayNew);
+        } else {
+            System.out.println("Index added is out of range");
+        }
+    }
+    private static String displayArray (int[] numbers) {
+        String display = "";
+        for (int number : numbers) {
+            display += number + "\t";
+        }
+        return display;
+    }
+
+    private static int[] arrayReplace (int[] numbers, int indexAdd, int valueAdd) {
+        int[] newArray = new int[numbers.length + 1];
+            for (int i = 0; i < indexAdd; i++) {
+                newArray[i] = numbers[i];
+            }
+            newArray[indexAdd] = valueAdd;
+            for (int j = indexAdd + 1; j < numbers.length + 1; j++) {
+                newArray[j] = numbers[j - 1];
+            }
+        return newArray;
+    }
+
+}
