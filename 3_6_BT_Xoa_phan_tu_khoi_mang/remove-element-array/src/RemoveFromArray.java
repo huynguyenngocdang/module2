4f8421758cdfb32964946d07681f50207f4ded2b
huynguyenngocdang <danghuy9229@gmail.com>
2023-09-25 12:54:45 +0700

    commit 3_6

---
+++ 3_6_BT_Xoa_phan_tu_khoi_mang/remove-element-array/src/RemoveFromArray.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class RemoveFromArray {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] numbers = {10, 4, 6, 7, 8, 6};
+        String choice;
+        do{
+
+        String displayOrigin = displayArray(numbers);
+        System.out.println("Origin array: " + displayOrigin);
+        System.out.println("Input value to remove");
+        int valReplace = scanner.nextInt();
+//        int[] numbersNew = removeArray(numbers, valReplace);
+//        String displayRemoved = displayArray(numbersNew);
+//        System.out.println("New array: " + displayRemoved);
+            numbers = removeArray(numbers, valReplace);
+        String displayRemoved = displayArray(numbers);
+        System.out.println("New array: " + displayRemoved);
+            System.out.println("Continue Y/N");
+            choice = scanner.next().toUpperCase();
+        } while (!choice.equals("N"));
+
+
+    }
+
+    private static String displayArray (int[] numbers) {
+        String display = "";
+        for (int i = 0; i < numbers.length; i++) {
+            display += numbers[i] + "\t";
+        }
+        return display;
+    }
+
+    private static int[] removeArray (int[] numbers, int valueReplace) {
+        int[] newArray = new int[numbers.length- countAppearArray(numbers,valueReplace)];
+        for (int i = 0, k =0; i < numbers.length; i++) {
+            if (numbers[i] != valueReplace) {
+                newArray[k] = numbers[i];
+                k++;
+            }
+        }
+        return newArray;
+    }
+
+    private static int countAppearArray (int[] numbers, int valueReplace) {
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == valueReplace) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
