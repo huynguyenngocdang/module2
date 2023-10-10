a47418acb57e275fca23a8801fa192e969b4d851
huynguyenngocdang <danghuy9229@gmail.com>
2023-10-10 08:25:31 +0700

    commit

--- 10_4_BT_Trien_khai_LinkedList/simple-linked-list/src/MyLinkedListTest.java
+++ 10_4_BT_Trien_khai_LinkedList/simple-linked-list/src/MyLinkedListTest.java
@@ -4,8 +4,17 @@ public class MyLinkedListTest {
         for (int i = 0; i < 11; i++) {
             llNumbers.add(i);
         }
+        System.out.println("Numbers contain");
         System.out.println(llNumbers.getFirst());
         System.out.println(llNumbers.getLast());
+        MyLinkedList<String> llStrings = new MyLinkedList<>();
+        for (int i = 0; i < 11; i++) {
+            llStrings.add(String.valueOf(i));
+        }
+        System.out.println("String contains");
+        System.out.println(llStrings.getFirst());
+        System.out.println(llStrings.getLast());
+        System.out.println(llStrings.get(5));
 
     }
 }
