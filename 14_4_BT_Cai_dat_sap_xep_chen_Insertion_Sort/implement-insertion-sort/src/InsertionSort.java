b9004f138cded6ce02b06649ec0b2be218b9faba
huynguyenngocdang <danghuy9229@gmail.com>
2023-10-11 15:50:54 +0700

    init commit

---
+++ 14_4_BT_Cai_dat_sap_xep_chen_Insertion_Sort/implement-insertion-sort/src/InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    static int[] list = {1, 9, 4, 6, 5, -4};
+    public static void insertionSort(int[] array){
+        int pos, x;
+        for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
+            x = array[i];
+            pos = i;
+            while(pos > 0 && x < array[pos-1]){
+                array[pos] = array[pos-1]; // đổi chỗ
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Array before sort");
+        for (int number: list
+             ) {
+            System.out.print(number + " ");
+        }
+        System.out.println("\nArray after sort");
+        insertionSort(list);
+        for (int number: list
+        ) {
+            System.out.print(number +" ");
+        }
+    }
+}
