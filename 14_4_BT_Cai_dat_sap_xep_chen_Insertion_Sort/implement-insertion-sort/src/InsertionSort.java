2286c297712b706effa4a6b098fca7f4687798b6
huynguyenngocdang <danghuy9229@gmail.com>
2023-10-11 16:10:18 +0700

    init commit

--- 14_4_BT_Cai_dat_sap_xep_chen_Insertion_Sort/implement-insertion-sort/src/InsertionSort.java
+++ 14_4_BT_Cai_dat_sap_xep_chen_Insertion_Sort/implement-insertion-sort/src/InsertionSort.java
@@ -3,6 +3,7 @@ public class InsertionSort {
     public static void insertionSort(int[] array){
         int pos, x;
         for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
+
             x = array[i];
             pos = i;
             while(pos > 0 && x < array[pos-1]){
